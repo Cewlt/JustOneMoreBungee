395db55ddcc29f839e53d06657d314b7d51ea47b
Colt <Cewlt@users.noreply.github.com>
2015-01-13 10:41:03 -0500

    Update JustOneMore.java

--- com/colt/JustOneMoreBungee/JustOneMore.java
+++ com/colt/JustOneMoreBungee/JustOneMore.java
@@ -10,16 +10,16 @@ import net.md_5.bungee.event.EventHandler;
 public class JustOneMore extends Plugin implements Listener {
   
   @Override
-  public void onEnable(){
-    this.getProxy().getPluginManager().registerListener(this, this);
-}
+	public void onEnable() {
+      	    this.getProxy().getPluginManager().registerListener(this, this);
+	}
 	
   @EventHandler
   public void onPing(ProxyPingEvent ev) {
-		ServerPing response = ev.getResponse();
-		Players players = response.getPlayers();
-		players = new Players(Math.min(players.getOnline() + 1, players.getMax()), players.getOnline(), players.getSample());
-		ServerPing ping = new ServerPing(response.getVersion(), players, response.getDescription(), response.getFaviconObject());
+		ServerPing r = ev.getResponse();
+		Players p = r.getPlayers();
+		p = new Players(Math.min(p.getOnline() + 1, p.getMax()), p.getOnline(), p.getSample());
+		ServerPing ping = new ServerPing(r.getVersion(), p, r.getDescription(), r.getFaviconObject());
 		ev.setResponse(ping);
 	}
 }
